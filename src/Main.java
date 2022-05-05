49571867895539ef6a838ff9d78e2eba8d8846f3
phil bridge <pbridge@qa.com>
2022-05-05 07:18:34 +0100

    Adding the additional conditional checks

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println("Yay short version says it is a Gnu");
         else
             System.out.println("It was not a Gnu so not a Gnu was the only other option");
-        
+
         String animal = "Dog";
 
         if(animal == "Cat")
@@ -24,5 +24,17 @@ public class Main {
             System.out.println("It's a Eombat");
         else
             System.out.println("It's a lovely dog");
+
+        int myNumber = 50;
+        if(myNumber == 50)
+            System.out.println("My number is 50");
+        if(myNumber < 100)
+            System.out.println("My number is less than 100");
+        if(myNumber > 1)
+            System.out.println("My number is greater than 1");
+        if(myNumber <= 50)
+            System.out.println("My number is either 50 or less");
+        if(myNumber >= 50)
+            System.out.println("my number is either 50 or more");
     }
 }
